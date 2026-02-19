c6048dec35ec776d3c81ba84e1cf47020b164d49
NobleXe <jakubsilakowski2008@gmail.com>
2026-02-19 23:12:22 +0100

    Made online communication possible

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const socket = io();
 const messageInput = document.getElementById('message');
 const sendButton = document.getElementById('send');
 const messageList = document.getElementById('messageList');
@@ -6,13 +7,18 @@ function sendMessage(event) {
     if(event.key === 'Enter' || event.type === 'click') {
         const message = messageInput.value;
         if (message.trim() !== '') {
-            const messageElement = document.createElement('div');
-            messageElement.textContent = `You: ${message}`;
-            messageList.appendChild(messageElement);
+            socket.emit('chat message', message);
+            
             messageInput.value = '';
         }
     }
 }
 
 sendButton.addEventListener('click', sendMessage);
+
+socket.on('chat message', function(msg) {
+    const messageElement = document.createElement('div');
+    messageElement.textContent = msg;
+    messageList.appendChild(messageElement);
+});
 
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+const { Server } = require('socket.io');
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', function(socket) {
+    console.log('A user connected');
+
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+app.use(express.static(__dirname));
 
 server.listen(3000, function() {
     console.log('Server is listening on port 3000');
 });
+
